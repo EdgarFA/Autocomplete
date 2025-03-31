644b0cdde57805d46117c567306532b29f14ce6e
Edgar Fernandez <efernandez@einatec.com>
2025-03-31 20:25:38 +0200

    add file

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Autocomplete",
+    platforms: [
+        .iOS(.v11) // o superior si quieres
+    ],
+    products: [
+        .library(
+            name: "Autocomplete",
+            targets: ["Autocomplete"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Autocomplete",
+            path: "Sources/Autocomplete"
+        )
+    ]
+)
